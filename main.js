a231ebd7f1d389dcf366e78a3d57c3b0e585ac61
Diego Tellez <diegotellezcontreras@gmail.com>
2022-10-18 14:18:32 -0500

    Tercer commit, clase 22

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
-const aside = document.querySelector(".product-detail")
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer")
 const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
@@ -11,20 +11,20 @@ menuHamIcon.addEventListener("click", toggleMobileMenu)
 menuCarritoIcon.addEventListener("click", toggleAside)
 
 function toggleDesktopMenu() {
-    const IsAsideClosed = aside.classList.contains("inactive")
+    const IsAsideClosed = shoppingCartContainer.classList.contains("inactive")
     
     if (!IsAsideClosed) {
-        aside.classList.add("inactive")
+        shoppingCartContainer.classList.add("inactive")
     }
     
     desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu() {
-    const IsAsideClosed = aside.classList.contains("inactive")
+    const IsAsideClosed = shoppingCartContainer.classList.contains("inactive")
     
     if (!IsAsideClosed) {
-        aside.classList.add("inactive")
+        shoppingCartContainer.classList.add("inactive")
     }
 
     mobileMenu.classList.toggle("inactive")
@@ -40,7 +40,7 @@ function toggleAside() {
         desktopMenu.classList.add("inactive")
     }
 
-    aside.classList.toggle("inactive")
+    shoppingCartContainer.classList.toggle("inactive")
     
     
 }
